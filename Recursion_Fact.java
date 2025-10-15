f29cdecb40ed05e8ee46242afc6f70793dd967df
S4N7H05H <santhoshadhi8754@gmail.com>
2025-10-15 13:49:10 +0530

    recursive fact

---
+++ Recursion_Fact.java
@@ -0,0 +1,14 @@
+public class Recursion_Fact{
+    static int fact(int n){
+        if(n == 0 || n == 1){
+            return 1;
+        } else {
+            return n * fact(n - 1);
+        }
+    }
+    public static void main(String[] args) {
+        int number = 5; 
+        int result = fact(number);
+        System.out.println("Factorial of " + number + " is: " + result);
+    }
+}
